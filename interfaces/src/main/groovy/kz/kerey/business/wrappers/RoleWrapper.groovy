81147f7375d790a6f88108fb5e90c2a32633cfd3
Dauren Mussa <dauren.mussa@gmail.com>
2015-03-17 18:48:01 +0200

    removed unused annotations from wrappers

--- interfaces/src/main/groovy/kz/kerey/business/wrappers/RoleWrapper.groovy
+++ interfaces/src/main/groovy/kz/kerey/business/wrappers/RoleWrapper.groovy
@@ -1,11 +1,5 @@
 package kz.kerey.business.wrappers
 
-import javax.xml.bind.annotation.XmlAccessType
-import javax.xml.bind.annotation.XmlAccessorType
-import javax.xml.bind.annotation.XmlRootElement
-
-@XmlRootElement
-@XmlAccessorType(XmlAccessType.FIELD)
 class RoleWrapper implements Serializable {
 
 	Long id
--- interfaces/src/main/groovy/kz/kerey/business/wrappers/TaskWrapper.groovy
+++ interfaces/src/main/groovy/kz/kerey/business/wrappers/TaskWrapper.groovy
@@ -2,13 +2,6 @@ package kz.kerey.business.wrappers
 
 import kz.kerey.business.types.enums.TaskStatus
 
-import javax.xml.bind.annotation.XmlAccessType
-import javax.xml.bind.annotation.XmlAccessorType
-
-import javax.xml.bind.annotation.XmlRootElement
-
-@XmlRootElement
-@XmlAccessorType(XmlAccessType.FIELD)
 class TaskWrapper implements Serializable {
 
 	Long id
